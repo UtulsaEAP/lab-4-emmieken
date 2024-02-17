ccbcd33361334f1c8845c687719631fa19f5da14
Emmie Kennemer <eek2654@utulsa.edu>
2024-02-16 20:11:30 -0600

    wrong syntax problem 1?

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -1,15 +1,19 @@
 """
 Complete the following python code to print the reverse binary representation of positive integer number entered by the user.
 
-Name:
-Lab Time:
+Name: Emmie Kennemer
+Lab Time: 2/16/24
 
 """
 
-
 def reverse_binary():
     user_num = int(input())
-    # YOUR CODE HERE
+
+    while user_num > 0:
+        user_num += 1    
+        print(user_num % 2)
+    
+
 
 if __name__ == "__main__":
     reverse_binary()
