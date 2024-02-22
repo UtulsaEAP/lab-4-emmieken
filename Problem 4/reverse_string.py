166eeda65f43cc4daee6d0cc1972276caae97d20
Emmie Kennemer <eek2654@utulsa.edu>
2024-02-22 17:13:08 -0600

    stuck on problem 4, mostly right

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -1,13 +1,21 @@
 """
 Complete the following python code to reverse the string entered by the user.
 
-Name: 
-Lab Time:
+Name: Emmie Kennemer
+Lab Time: 2/22/24
 """
 
 def reverse_string():
-    # YOUR CODE HERE
+    phrase1 = str(input())
+    phrase2 = str(input())
+
+    for i in range(len(phrase1) -1, 0-1, -1):
+        print(phrase1[i], end = "")
     
+    for i in range(len(phrase2) -1, 0-1, -1):
+        print(phrase2[i], end = "")
+
+
 
 if __name__ == "__main__":
-    reverse_string()
+        reverse_string()        
