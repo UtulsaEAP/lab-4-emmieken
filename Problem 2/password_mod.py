2b62db4559d1844774c41da6f644944fa2bca72e
Emmie Kennemer <eek2654@utulsa.edu>
2024-02-22 13:40:08 -0600

    attempted problems 1 and 2.... need help

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,14 +1,29 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Emmie Kennemer
+Lab Time: 2/22/24
 """
 
 def password_mod():
-    word = input()
-    password = ''
-    # Type your code here.
+    password = input()
+    
+    for i in password:
+        if i == "i":
+            password += "1"
+        elif i == "a":
+            password += "@"
+        elif i == "m":
+            password += "M"
+        elif i == "B":
+            password += "8"
+        elif i == "s":
+            password += "$"
+        else:
+            password += i
+        
+    
+    print(str(password) + "!")
 
 if __name__ == "__main__":
     password_mod()
